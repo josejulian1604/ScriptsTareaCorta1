685e7a155e57e5ba185a43ced1ec8dac952a297a
VictorNunez911 <112830540+VictorNunez911@users.noreply.github.com>
2022-09-06 21:44:10 -0600

    Update FiltrarArticulosPorCantidad.sql

--- Scripts/FiltrarArticulosPorCantidad.sql
+++ Scripts/FiltrarArticulosPorCantidad.sql
@@ -1,6 +1,6 @@
 USE [Tarea1]
 GO
-/****** Object:  StoredProcedure [dbo].[FiltrarArticulosPorCantidad]    Script Date: 4/9/2022 19:05:55 ******/
+/****** Object:  StoredProcedure [dbo].[FiltrarArticulosPorCantidad]    Script Date: 6/9/2022 21:30:31 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,25 +8,32 @@ GO
 -- =============================================
 -- Author:		<Jose Julian Gutierrez, Victor Nuñez>
 -- Create date: <4/9/2022>
--- Description:	<SP que consiste en Filtrar los articulos por su precio, escribiendo una cantidad especifica>
+-- Description:	<SP que consiste en Desplegar los primeros N elementos, este N es proporcionado por ell usuario>
 -- =============================================
+
 ALTER PROCEDURE [dbo].[FiltrarArticulosPorCantidad]
 	
-	@Cantidad VARCHAR(128)
+	@Cantidad INT
 	
 AS
 BEGIN
 
 	SET NOCOUNT ON;
 
-	SELECT A.id, A.Nombre AS Articulo, A.Precio, C.Nombre AS ClaseArticulo  --Desplieque de las tablas
+	SELECT A.id, A.Nombre AS Articulo, A.Precio, C.Nombre AS ClaseArticulo
+
+
+	--Esta parte se encarga de ligar las tablas
 
 	FROM Articulo A
-		INNER JOIN ClaseArticulo C ON C.id = A.idClaseArticulo -- Permite ligar las tablas
+		INNER JOIN ClaseArticulo C ON C.id = A.idClaseArticulo 
 
-	WHERE (A.Precio LIKE '%' + @Cantidad + '%') -- Compara lo solicitado por el usuario con todos los precios de las tablas
-	
-	ORDER BY Articulo -- Lo ordena por orden alfabetico
+
+	-- Estas lineas ordenan y delimitan los articulos mostrados
+
+	ORDER BY Articulo
+	OFFSET 1 ROW
+	FETCH NEXT @Cantidad ROWS ONLY
 
 
 END
